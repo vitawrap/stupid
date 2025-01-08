997a0688ce40332a8086740f36790b40351d4735
vitawrap <thevitawrap@gmail.com>
2025-01-08 12:15:01 +0100

    Main: Lighting and render loop

--- Spaceship/main.js
+++ Spaceship/main.js
@@ -12,6 +12,9 @@ let scene;
 /** @type {THREE.Camera} */
 let camera;
 
+/** @type {THREE.Object3D} */
+let object;
+
 function gameInitialize() {
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(75);
@@ -26,15 +29,23 @@ function gameInitialize() {
 
     const loader = new PLYLoader();
     loader.load("Spaceship/assets/ship.ply", (buf) => {
-        let object = new THREE.Mesh(buf);
+        const material = new THREE.MeshPhongMaterial();
+        object = new THREE.Mesh(buf, material);
         scene.add(object);
         object.position.z = -4
-        object.rotateX(3);
+        object.rotateX(2.4);
         object.rotateY(0.5);
     });
+
+    const amb = new THREE.AmbientLight( 0x404040 );
+    const sun = new THREE.DirectionalLight( 0xFFFFFF, 0.8 );
+    scene.add( amb, sun );
 }
 
 function gameAnimate() {
+    if (object != undefined)
+        object.rotateY(0.1);
+
     renderer.render(scene, camera);
 }
 
